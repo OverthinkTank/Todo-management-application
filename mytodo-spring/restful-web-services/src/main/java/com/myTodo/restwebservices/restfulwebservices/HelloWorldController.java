498107abc09535c029c151d102e733dd30ba5aa5
Vivek Patel <40636968+OverthinkTank@users.noreply.github.com>
2020-08-31 18:12:54 -0700

    restful-web-service + java  + spring-boot

---
+++ mytodo-spring/restful-web-services/src/main/java/com/myTodo/restwebservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,39 @@
+package com.myTodo.restwebservices.restfulwebservices;
+import java.util.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+//To resolve CORS error
+@CrossOrigin(origins = "http://localhost:4200")
+//https://localhost:4200
+
+public class HelloWorldController {
+	
+	//GET
+	//URI - /hello-world  
+	//method - "HelloWorld"
+
+//	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
+	public String HelloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
+	}
+	
+	//"/hello-world/path-variable/Vwake"
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+}
