404d0837d4933f10fc0d167ccaf80a6cc01634fe
Vivek Patel <40636968+OverthinkTank@users.noreply.github.com>
2020-09-09 17:29:45 -0700

    Added Spring Boot REST API - DELETE Request

--- mytodo-spring/restful-web-services/src/main/java/com/myTodo/restwebservices/restfulwebservices/todo/ToDoResource.java
+++ mytodo-spring/restful-web-services/src/main/java/com/myTodo/restwebservices/restfulwebservices/todo/ToDoResource.java
@@ -3,7 +3,9 @@ package com.myTodo.restwebservices.restfulwebservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +19,24 @@ public class ToDoResource {
 	@Autowired
 	private ToDoHardCodedService todoService;
 	
+/*	GET	 */
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllToDos(@PathVariable String username) throws InterruptedException{
 //		Thread.sleep(3000);	
 		return todoService.findAll();
 		
+	}
+	
+/*	DELETE  */
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteToDo(@PathVariable String username, @PathVariable long id){
+		
+		Todo todo = todoService.deleteById(id);
+		System.out.println(todo.isDone());
+		if(todo!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
